ae2bcf97be40a62d704e54a81a135f3fa983187c
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-26 16:22:01 +0100

    Fix indentation add license link to the license section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -24,16 +24,16 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   switch (license) {
     case "MIT":
-      return `(https://opensource.org/licenses/MIT)`;
+      return "https://opensource.org/licenses/MIT";
 
     case "GPLv3":
-      return `(https://www.gnu.org/licenses/gpl-3.0)`;
+      return "https://www.gnu.org/licenses/gpl-3.0";
 
     case "Apache 2.0":
-      return `(https://opensource.org/licenses/Apache-2.0)`;
+      return "https://opensource.org/licenses/Apache-2.0";
 
     case "BSD 3-Clause":
-      return `(https://opensource.org/licenses/BSD-3-Clause)`;
+      return "https://opensource.org/licenses/BSD-3-Clause";
 
     default:
       return "";
@@ -45,16 +45,16 @@ function renderLicenseLink(license) {
 function renderLicenseSection(license) {
   switch (license) {
     case "MIT":
-      return "This project is licensed under the MIT License. You are free to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software, provided that the original copyright notice and permission notice are included in all copies or substantial portions of the software. For more details, see the [LICENSE](LICENSE) file.";
+      return "This project is licensed under the MIT License. You are free to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software, provided that the original copyright notice and permission notice are included in all copies or substantial portions of the software.";
 
     case "GPLv3":
-      return "This project is licensed under the GPLv3 License. You are free to use, copy, distribute, and modify the software, provided that any derivative work is also open-source and distributed under the same license. For more details, see the [LICENSE](LICENSE) file.";
+      return "This project is licensed under the GPLv3 License. You are free to use, copy, distribute, and modify the software, provided that any derivative work is also open-source and distributed under the same license.";
 
     case "Apache 2.0":
-      return "This project is licensed under the Apache 2.0 License. You are free to use, copy, modify, and distribute the software, both in open-source and proprietary projects, provided that a copy of the license, a notice of any changes made, and an explicit grant of patent rights are included. For more details, see the [LICENSE](LICENSE) file.";
+      return "This project is licensed under the Apache 2.0 License. You are free to use, copy, modify, and distribute the software, both in open-source and proprietary projects, provided that a copy of the license, a notice of any changes made, and an explicit grant of patent rights are included.";
 
     case "BSD 3-Clause":
-      return "This project is licensed under the BSD 3-Clause License. You are free to use, copy, modify, and distribute the software, provided that minimal attribution is given and the names of the contributors are not used to promote derived products. For more details, see the [LICENSE](LICENSE) file.";
+      return "This project is licensed under the BSD 3-Clause License. You are free to use, copy, modify, and distribute the software, provided that minimal attribution is given and the names of the contributors are not used to promote derived products.";
 
     default:
       return "";
@@ -80,11 +80,11 @@ function generateMarkdown(data) {
 
   ${renderLicenseBadge(license)}
     
-   ## Description
+## Description
    
-   ${description}
+${description}
 
-   ## Table of Contents
+## Table of Contents
 
 - [Installation](#installation)
 - [Usage](#usage)
@@ -93,45 +93,50 @@ function generateMarkdown(data) {
 - [Tests](#tests)
 - [Questions](#questions)
     
-   ## Installation
+## Installation
    
-   To install the necessary dependencies, run the following command:
+To install the necessary dependencies, run the following command:
 
-   \`\`\`sh
-   ${installation}   
-   \`\`\`
+ \`\`\`sh
+${installation}   
+\`\`\`
 
-   ## Usage
+## Usage
 
-   To use this application, run the following command:
+To use this application, run the following command:
 
-   \`\`\`sh
-   ${usage}   
-   \`\`\`
+\`\`\`sh
+${usage}   
+\`\`\`
 
-   ## Contributing
+## Contributing
 
-   ${contributing}
+${contributing}
 
-   ## License
+## License
 
-   ${renderLicenseSection(license)}
+${renderLicenseSection(
+  license
+)} For more details, see the <a href="https://github.com/${renderLicenseLink(
+    license
+  )}">${license}</a> file. 
 
-   ## Tests
 
-   To run tests, use the following command:
+## Tests
 
-   \`\`\`sh
-   ${tests}   
-   \`\`\`
+To run tests, use the following command:
+
+\`\`\`sh
+${tests}   
+\`\`\`
    
-   ## Questions
+## Questions
 
-   If you have any questions about the project, please feel free to contact me directly:
+If you have any questions about the project, please feel free to contact me directly:
 
-   GitHub: <a href="https://github.com/${github}">${github}</a>
+GitHub: <a href="https://github.com/${github}">${github}</a>
 
-   Email: <a href="mailto:${email}">${email}</a>
+Email: <a href="mailto:${email}">${email}</a>
 `;
 }
 
