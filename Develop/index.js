9235ba7ab57316aea965a63fc40d6fdfeb0c3cfa
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-26 15:23:31 +0100

    Correct the file path to the generateMarkdown.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,6 @@
 import inquirer from "inquirer";
 import fs from "fs";
-import generateMarkdown from "./utils/generateMarkdown";
+import generateMarkdown from "./utils/generateMarkdown.js";
 
 // TODO: Create an array of questions for user input
 const questions = [
