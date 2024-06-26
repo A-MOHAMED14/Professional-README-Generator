8ab81f0bd578728df404cfcde0a4c03a0ff0934c
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-26 14:11:07 +0100

    Import generateMarkdown from generateMarkdown.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 import inquirer from "inquirer";
 import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown";
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -53,37 +54,6 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fileName = "README.md";
-
-  data = `# ${title}
-    
-   ## Description
-   
-   ${description}
-    
-   ## Installation
-
-   ${installation}
-
-   ## Usage
-
-   ${usage}
-
-   ## How to Contribute
-
-   ${contributing}
-
-   ## License
-
-   ${license}
-
-   ## Questions
-
-   <a href="https://github.com/${github}">GitHub</a>
-
-   If you have further question, <a href="mailto:${email}">shoot me an email</a>
-    `;
-
   fs.writeFile(fileName, data, (err) => {
     err ? console.error(err) : console.log("README file created successfully!");
   });
