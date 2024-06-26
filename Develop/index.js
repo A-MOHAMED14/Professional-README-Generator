b1ef7886e38ca7e068fb17efcca708c1ef2838c3
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-26 13:48:04 +0100

    Create a function to initialise app

--- Develop/index.js
+++ Develop/index.js
@@ -90,7 +90,11 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    writeToFile("README.md", responses);
+  });
+}
 
 // Function call to initialize app
 init();
