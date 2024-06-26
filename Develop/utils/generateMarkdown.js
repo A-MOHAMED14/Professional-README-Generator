0b77050eb4750de16a074bcb9bbce906967b391d
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-26 17:52:54 +0100

    Add code comments

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Returns a license badge based on the given license, or an empty string if none
 function renderLicenseBadge(license) {
   switch (license) {
     case "MIT":
@@ -19,8 +18,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Returns the license link for the given license, or an empty string if none
 function renderLicenseLink(license) {
   switch (license) {
     case "MIT":
@@ -40,8 +38,7 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// Returns the license section of the README, or an empty string if none
 function renderLicenseSection(license) {
   switch (license) {
     case "MIT":
@@ -61,7 +58,7 @@ function renderLicenseSection(license) {
   }
 }
 
-// TODO: Create a function to generate markdown for README
+// Generates markdown for README using provided data
 function generateMarkdown(data) {
   const {
     title,
