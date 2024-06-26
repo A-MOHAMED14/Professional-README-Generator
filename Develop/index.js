90b5f846b60b97d85b0ebf0c04efc047bba0765e
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-26 13:02:10 +0100

    Create an array of questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,17 @@ import inquirer from "inquirer";
 import fs from "fs";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What is the title of your project?",
+  "Provide a description of your project:",
+  "Provide installation instructions:",
+  "Provide usage information:",
+  "Provide contribution guidelines:",
+  "Provide test instructions:",
+  "Choose a license for your project:",
+  "What is your GitHub username?",
+  "What is your email address?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
